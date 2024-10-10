0f7aee83d6ec335bee46857c675e049c71fd28f0
nancy,yun-lin,hsu <67849065+NancyHsu1994Taiwan@users.noreply.github.com>
2024-10-11 07:30:08 +0800

    feat: 移除views設定

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ import usersRouter from './routes/users.js'
 var app = express();
 
 // view engine setup
-const __dirname = path.resolve()
+// const __dirname = path.resolve()
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
